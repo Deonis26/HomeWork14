4b34512112a112e1918a1dbd61ae61bc7c7c0b76
Denis <85250266+Deonis26@users.noreply.github.com>
2023-09-01 22:19:41 +0300

    объявление полей в классе train. Заполнение файла READ.me

---
+++ src/src/Train/Train.java
@@ -0,0 +1,11 @@
+package Train;
+
+public class Train {
+    String numberTrain; // номер поезда
+    String route; // маршрут
+    int distanceBetweenCities; //расстояние между городами
+    String status;//статус
+    int quantityLiter; //количество литров
+    int fuelConsumption; // расход топлива на 100 км
+
+}
